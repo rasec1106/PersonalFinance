3d4f1d87468b7a5a2c8f619bd168b6c9e14e22f1
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-06-04 10:05:00 -0500

    First project structure

---
+++ app/src/main/java/pe/edu/cibertec/personalfinance/MainActivity.kt
@@ -0,0 +1,43 @@
+package pe.edu.cibertec.personalfinance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.cibertec.personalfinance.ui.theme.PersonalFinanceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PersonalFinanceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PersonalFinanceTheme {
+        Greeting("Android")
+    }
+}
