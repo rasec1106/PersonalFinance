b42fbf8fdc54542149c90af18ce8f5bb14d54a64
rasec1106 <elch.herrera@gmail.com>
2023-07-01 15:58:03 -0500

    Create remote data structure

---
+++ app/src/main/java/pe/edu/cibertec/personalfinance/data/remote/service/EntryService.kt
@@ -0,0 +1,10 @@
+package pe.edu.cibertec.personalfinance.data.remote.service
+
+import pe.edu.cibertec.personalfinance.data.model.Entry
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface EntryService {
+    @GET("entries")
+    fun getEntries(): Call<Entry>
+}
